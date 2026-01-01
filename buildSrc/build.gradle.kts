22f30ff6b5a48dece41b4dbda2016c153e23647c
CDAGaming <cstack2011@yahoo.com>
2026-01-01 14:40:02 -0600

    [Change] Updated ASM and Licensing to 2026

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,9 +7,9 @@ repositories {
 }
 
 dependencies {
-    implementation("org.ow2.asm:asm:9.7.1")
-    implementation("org.ow2.asm:asm-commons:9.7.1")
-    implementation("org.ow2.asm:asm-tree:9.7.1")
-    implementation("org.ow2.asm:asm-util:9.7.1")
-    implementation("org.ow2.asm:asm-analysis:9.7.1")
+    implementation("org.ow2.asm:asm:9.9.1")
+    implementation("org.ow2.asm:asm-commons:9.9.1")
+    implementation("org.ow2.asm:asm-tree:9.9.1")
+    implementation("org.ow2.asm:asm-util:9.9.1")
+    implementation("org.ow2.asm:asm-analysis:9.9.1")
 }
